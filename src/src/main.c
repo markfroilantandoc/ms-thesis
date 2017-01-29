a752874a4d9f6e620e3710630fde642f565d06ca
markfroilantandoc <markfroilantandoc@gmail.com>
2017-01-29 19:18:31 +0800

    cleaning

---
+++ src/src/main.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+int main (int argc, char* argv[]) {
+
+    FILE *fp;
+    int i;
+    float tempx, tempy;
+    int num_nodes;
+
+    if (argc < 2) {
+        return 1;
+    }
+    fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        return 1;
+    }
+
+    ENopen ("", "", "");
+
+    // Scan terminals
+    fscanf(fp, "%d\n", &num_nodes);
+    for (i=0; i<num_nodes; i++) {
+        fscanf(fp, "%f %f\n", &tempx, &tempy);
+    }
+
+    // Compute Steiner tree
+
+    // Compute feasibility of network
+
+    // Display output
+
+    ENclose();
+    fclose(fp);
+
+    return 0;
+
+}
