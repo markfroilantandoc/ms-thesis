a98a618f2601fcd5d1bb7fed8c9fad6f5dae5f59
Froilan Tandoc <markfroilantandoc@gmail.com>
2017-02-24 15:37:23 +0800

    flow computation attempt

--- src/src/fluidDynamicsUtil.c
+++ src/src/fluidDynamicsUtil.c
@@ -45,11 +45,7 @@ void computeFlow(network *water_network, int **adjacency_matrix)
 
 double computeFlowLink(int index1, int index2, network *water_network, int **adjacency_matrix, int *visited)
 {
-    int i, index, flag = 1;
-    //printf("%d %d\n", index1, index2);
-    if (index2 < index1) {
-        flag = -1;
-    }
+    int i, index;
     for (i=0; i<water_network->pipe_count; i++) {
         if ((water_network->links[i].n1 == index1 && water_network->links[i].n2 == index2) ||
             (water_network->links[i].n2 == index1 && water_network->links[i].n1 == index2)) {
@@ -57,17 +53,21 @@ double computeFlowLink(int index1, int index2, network *water_network, int **adj
             break;
         }
     }
-    visited[i] = 1;
+
+    visited[index] = 1;
     for (i=0; i<water_network->pipe_count; i++) {
         if (water_network->links[i].n1 == index2 && !visited[i]) {
-            water_network->links[index].flow -= computeFlowLink(index2, water_network->links[i].n2, water_network, adjacency_matrix, visited);
+            water_network->links[index].flow -= abs(computeFlowLink(index2, water_network->links[i].n2, water_network, adjacency_matrix, visited));
         } else if (water_network->links[i].n2 == index2 && !visited[i]) {
-            water_network->links[index].flow += computeFlowLink(index2, water_network->links[i].n1, water_network, adjacency_matrix, visited);
+            water_network->links[index].flow += abs(computeFlowLink(index2, water_network->links[i].n1, water_network, adjacency_matrix, visited));
         }
     }
     water_network->links[index].flow -= water_network->junctions[index1].flow;
     water_network->links[index].flow += water_network->junctions[index2].flow;
-    printf("\t %d %d\n", index1, index2);
+    if (index2 < index1)
+    {
+        water_network->links[index].flow *= -1;
+    }
     return water_network->links[index].flow;
 }
 
@@ -75,10 +75,15 @@ void computeFlowNew(network *water_network, int **adjacency_matrix)
 {
     int i;
     int *visited = NULL;
-    visited = (int *)malloc(water_network->node_count * sizeof(int));
-    for (i=0; i<water_network->node_count; i++) {
+    visited = (int *)malloc(water_network->pipe_count * sizeof(int));
+    for (i=0; i<water_network->pipe_count; i++) {
         visited[i] = 0;
     }
-    computeFlowLink(0, 5, water_network, adjacency_matrix, visited);
+    for (i=0; i<water_network->pipe_count; i++) {
+        if (water_network->links[i].n1 == 0) {
+            computeFlowLink(0, water_network->links[i].n2, water_network, adjacency_matrix, visited);
+            break;
+        }
+    }
     free(visited);
 }
--- src/src/fluidDynamicsUtil.h
+++ src/src/fluidDynamicsUtil.h
@@ -1,6 +1,7 @@
 #include "types.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 void static computeFlowDFS(int i, network *water_network, node *temp_junctions, int *visited, int **adjacency_matrix);
 void computeFlow(network *water_network, int **adjacency_matrix);
